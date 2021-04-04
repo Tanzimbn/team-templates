afaf3b744117847282776e7f5de1a8239608d1c8
Asad-Bin <66724153+Asad-Bin@users.noreply.github.com>
2021-04-04 12:14:39 +0600

    Update Euler's totent function.cpp

--- Number Theory/Euler's totent function.cpp
+++ Number Theory/Euler's totent function.cpp
@@ -1,5 +1,34 @@
 // Euler's Totient
 
+vector<int> primes;
+bitset<100005> mark;
+void sieve(int n)
+{
+    int i, j, limit = sqrt(n*1.) + 2;
+    
+    mark.reset();
+    mark[1] = 1;
+
+    for(i = 4; i<=n; i+=2) mark[i] = 1;
+
+    primes.push_back(2);
+
+    for(i = 3; i <= n; i += 2)
+    {
+        if(!mark[i])
+        {
+            primes.push_back(i);
+
+            if(i<=limit)
+            {
+                for(j = i*i; j <= n; j += i*2)
+                {
+                    mark[j] = 1;
+                }
+            }
+        }
+    }
+}
 int EulerT(int n) {
     if(n==1)return 0;
     int cnt, toi=n, i;
