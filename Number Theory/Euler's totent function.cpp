74c502a35e160ba6bbc6f1f351debbc745c06bca
Asad-Bin <66724153+Asad-Bin@users.noreply.github.com>
2021-04-12 09:30:54 +0600

    Update Euler's totent function.cpp

--- Number Theory/Euler's totent function.cpp
+++ Number Theory/Euler's totent function.cpp
@@ -31,8 +31,8 @@ void sieve(int n)
 }
 int EulerT(int n) {
     if(n==1)return 0;
-    int cnt, toi=n, i;
-    for(i=0; i<primes.size() && (primes[i]*primes[i])<=n; i++) {
+    int toi=n, i;
+    for(i=0; i<(int)primes.size() && (primes[i]*primes[i])<=n; i++) {
         if(n%primes[i]==0) {
         while(n%primes[i]==0) n/=primes[i];
             toi-=toi/primes[i];
