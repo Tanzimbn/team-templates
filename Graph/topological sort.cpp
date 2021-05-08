0f7bdc84e05f86a8bbdea9458cf1ef339020084f
Asad-Bin <66724153+Asad-Bin@users.noreply.github.com>
2021-05-08 11:35:21 +0600

    Update topological sort.cpp

--- Graph/topological sort.cpp
+++ Graph/topological sort.cpp
@@ -1,33 +1,32 @@
-vi ara[1005], in_deg(1005);
-vector<pii> order;
+const int MX = 1e4;
+vi ara[MX+5], in_deg(MX+5);
+vector<int> order;
 void topsort(int n)
 {
-	priority_queue<pii> q;
-	// maybe a queue is enough sometimes ???
+	queue<int> q;
+	// maybe a queue is enough sometimes or need priority queue???
 	order.clear();
 	
-	for(int K=0; K<n; K++){
+	for(int K=1; K<=n; K++){
 		if(!in_deg[K]){
-			q.push({-K, 1});
+			q.push(K);
 			// As want to proccess smaller first
 			// multiplying with -1 makes the small number bigger(!)
 		}
 	}
 	
 	while(!q.empty()){
-		pii u = q.top();
+		int u = q.front();
 		q.pop();
 		
-		u.fi *= -1;
-		
-		order.PB({u.se, u.fi});
+		order.PB(u);
 		// updating solution list
 		
-		for(int K=0; K<sz(ara[u.fi]); K++){
-			in_deg[ara[u.fi][K]]--;
-			
-			if(!in_deg[ara[u.fi][K]]){
-				q.push({-1*ara[u.fi][K], u.se+1});
+		for(int K=0; K<sz(ara[u]); K++){
+			in_deg[ara[u][K]]--;
+
+			if(in_deg[ara[u][K]] == 0){
+				q.push(ara[u][K]);
 				// child node's level is 1 step lower
 				// so have to add 1 to parent node's level;
 			}
