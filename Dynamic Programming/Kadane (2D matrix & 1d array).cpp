0addc8478db4f5ca9364866a51de8d3b73053bf7
Asad Bin Saber <66724153+Asad-Bin@users.noreply.github.com>
2022-03-28 11:55:58 +0600

    Update Kadane (2D matrix & 1d array).cpp

--- Dynamic Programming/Kadane (2D matrix & 1d array).cpp
+++ Dynamic Programming/Kadane (2D matrix & 1d array).cpp
@@ -1,21 +1,10 @@
-
 // Kadane's algo for Maximum Sub sub-Matrix (including MS sub-array)...
-
-
 vector<vector<int> > ara(n, vector<int> (n));
-
 void MaxSumSubMatrix(int n)
 {
     int max_sum=0, sum, max_;
-
-    for(int K=0; K<n; K++){
-        for(int L=0; L<n; L++){
-            cin >> ara[K][L];
-        }
-    }
-
+    
     //test_input(n);
-
     int ansup=0, ansdown=0, ansleft=0, ansright=0, ul, dl, u, v;
     for(int K=0; K<n; K++){
         vector<int> help(n);
@@ -23,7 +12,7 @@ void MaxSumSubMatrix(int n)
             for(int M=0; M<n; M++){
                 help[M] += ara[M][L];
             }
-
+    
             // Kadane's Algo for a 1D single array...
             sum = max_ = help[0];
             ul = dl = u = v = 0;
@@ -38,7 +27,6 @@ void MaxSumSubMatrix(int n)
                     u = M;
                     v = M;
                 }
-
                 //max_ = max(max_, sum);
                 if(sum>max_){
                     max_ = sum;
@@ -59,4 +47,24 @@ void MaxSumSubMatrix(int n)
 
     cout << "\n" << max_sum << '\n';
     cout << ansleft << ' ' << ansright << ' ' << ansup << ' ' << ansdown << "\n";
+}
+
+
+// KADANE (1D)
+sum = ans = ara[0];
+int u = 0, v = 0;
+for(int K=1; K<n; K++){
+    if(sum+ara[K] >= ara[K]){
+        sum += ara[K];
+        v++;
+    }
+    else{
+        sum = ara[K];
+        u = v = K;
+    }
+    if(sum > ans){
+        ans = sum;
+        ans_l = u;
+        ans_r = v;
+    }
 }
