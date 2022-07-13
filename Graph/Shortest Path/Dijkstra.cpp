9dff8001fc9764d232a9d1650f9c2c2cec911db7
Asad Bin Saber <66724153+Asad-Bin@users.noreply.github.com>
2022-07-13 08:54:51 +0600

    Update Dijkstra.cpp

--- Graph/Shortest Path/Dijkstra.cpp
+++ Graph/Shortest Path/Dijkstra.cpp
@@ -1,53 +1,25 @@
-struct Node{
-	int at, cost;
-	
-	Node(int _at, int _cost){
-		at = _at;
-		cost = _cost;
-	}
-};
-bool operator<(Node A, Node B){
-	// Priority queue returns the greatest value
-	// So we need to write the comparator in a wayy
-	// So that cheapest value bacomes greatest value
-	return A.cost > B.cost;
-}
-struct Edge {
-	int v, w;
-	
-	Edge(int _v, int _w){
-		v = _v;
-		w = _w;
-	}
-};
+vector<pair<int, int> > edge[MX+5];
+int dist[MX+5];
 
-vector<Edge> adj[1005];               // adjacency list of weighted graph
-priority_queue<Node> PQ;
-int dist[101];
-int n;
-int dijkstra(int source, int dest)
+void dijkstra(int src, int d, int n)
 {
 	memset(dist, -1, sizeof dist);
-	
-	dist[source] = 0;
-	while(!PQ.empty()) PQ.pop();
-	PQ.push(Node(source, 0));
+	priority_queue<pair<int, int> > PQ;
+	PQ.push({0, src});
+	dist[src] = 0;
 	
 	while(!PQ.empty()){
-		Node u = PQ.top();
+		int cost = -1*PQ.top().first;
+		int u = PQ.top().second;
 		PQ.pop();
 		
-		if(u.cost!=dist[u.at]){
-			continue;
-		}
-		
-		for(Edge e:adj[u.at]){
-			if(dist[e.v]==-1 || dist[e.v] > u.cost+e.w){
-				dist[e.v] = u.cost+e.w;
-				PQ.push(Node(e.v, dist[e.v]));
+		for(auto e:edge[u]){
+			if(dist[e.first] == -1 || dist[e.first] > max(cost, e.second)){
+				dist[e.first] = max(cost, e.second);
+				PQ.push({-1*dist[e.first], e.first});
 			}
 		}
 	}
 	
-	return dist[dest];
+	return dist[d];
 }
